b43ac7ea1419b2cfa50ac28f1ae930ba5da87fac
Assil Ben Amor <massilbenamor@gmail.com>
2021-04-15 10:47:20 +0300

    First round of feedback

--- 7_indicators_YS.R
+++ 7_indicators_YS.R
@@ -13,29 +13,44 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 data <- read.csv("DSA_aggregated_data.csv",stringsAsFactors = F)
 
+data[data=="dnk"] <- NA
+data[data=="NC"] <- NA
 
 data_indicators <- data %>% mutate (
   ##### SNFI - Non Critical##### 
   
   #### SNFI index 1 
   ###YS should be the otherway round, 1 is "no" and 0 is "yes".
+  ###ASSIL Done
   snfi_nc_index1 = case_when(
-    nfi_access == "no" ~ 0,
-    nfi_access == "yes" ~ 1,
+    nfi_access == "no" ~ 1,
+    nfi_access == "yes" ~ 0,
   ),
   
   #### SNFI index 2
   ###YS the list was short listed to 7 items only for SNFI; would it be easier to the sum of the 7 dummies variables?
+  ###ASSIL corrected
+
   snfi_nc_index2 = case_when(
     
-      str_count(nfi_items_available,pattern = " ") - 
-        ifelse(sm_selected(nfi_items_available,any=c("none")),1,0) >=  5  ~ 1 ,
-      
-      str_count(nfi_items_available,pattern = " ") - 
-        ifelse(sm_selected(nfi_items_available,any=c("none")),1,0)  >= 1  ~ 0 
+    rowSums(across(c(nfi_items_available.sleep_mats,
+                     nfi_items_available.plastic_sheets,
+                     nfi_items_available.blankets,
+                     nfi_items_available.jerry_cans_buckets,
+                     nfi_items_available.cooking_utensils,
+                     nfi_items_available.mosquito_nets,
+                     nfi_items_available.solar_lamp)),na.rm = T) >=  5  ~ 1 ,
+    
+    rowSums(across(c(nfi_items_available.sleep_mats,
+                     nfi_items_available.plastic_sheets,
+                     nfi_items_available.blankets,
+                     nfi_items_available.jerry_cans_buckets,
+                     nfi_items_available.cooking_utensils,
+                     nfi_items_available.mosquito_nets,
+                     nfi_items_available.solar_lamp)),na.rm = T) >= 1 ~ 0 
+    
     
   ),
-  
   #### SNFI index 3
   ###YS ok
   snfi_nc_index3 = case_when(
@@ -71,7 +86,9 @@ data_indicators <- data %>% mutate (
   #### SNFI index 7
   ###YS the indicators speaks of the number of % of type of houses; the indicator used is the select multiple
   #should it be the buul, solid_apartment, unfinished, make_shit, none which are the % of the households?
-  snfi_nc_index7 = case_when(
+  
+  ###ASSIL I think the % in here means the number of a set of options selected par rapport all of the options 
+   snfi_nc_index7 = case_when(
     rowSums(across(c(shelter_types.buul,shelter_types.tent,shelter_types.shelter_kit)))/
       rowSums(across(starts_with("shelter_types."))) > 0.5 ~ 1,
     
@@ -84,6 +101,10 @@ data_indicators <- data %>% mutate (
   
   #### SNFI index 8
   ###YS why is 0 coded as NA; have the prop equals to zero should mean 0; hence 0 and not NA? 
+  ###ASSIl 0 is not coded as NA, actually none of the shelter_flood_destroyed or shelter_fire_destroyed
+  # is equal to 0 so the sum is always >0. But when both of the variables are NA and then I divide by
+  # cccm_populationestimates_shelters the result is 0 (run this sum(c(NA,NA),na.rm = T)/100) 
+  # when it should be NA
   snfi_nc_index8 = case_when(
   
     rowSums(across(c(shelter_flood_destroyed,shelter_fire_destroyed)),na.rm = T)/
@@ -96,7 +117,8 @@ data_indicators <- data %>% mutate (
       cccm_populationestimates_shelters > 0.25 ~ 1,
    
   ),
-    
+
+  #) %>% select(shelter_flood_destroyed,shelter_fire_destroyed,snfi_nc_index8) %>% View()
   
   
   #### SNFI index 9
@@ -109,14 +131,15 @@ data_indicators <- data %>% mutate (
   #### WASH index 1
   ###YS river and other are missing
   #could share the list of improved and un-improved water sources.
+  ###ASSIL I shared the list on the excel file and updated the vectors here 
   wash_index1 = case_when(
     sm_selected(water_treatment_methods,none=c("boiling", "cloth_filter", "other_filter", "aquatabs") ) &
-      water_sources_primary %in% c("unprot_well", "berkad", "water_tank_tap", "water_trucking_distrib", "borehole_pump")  ~ 3,
+      water_sources_primary %in%  c("unprot_well", "berkad", "river") ~ 3,
     
     sm_selected(water_treatment_methods,any=c("boiling", "cloth_filter", "other_filter", "aquatabs") ) &
-      water_sources_primary %in% c("unprot_well", "berkad", "water_tank_tap", "water_trucking_distrib", "borehole_pump")  ~ 2,
+      water_sources_primary %in% c("unprot_well", "berkad", "river")  ~ 2,
     
-    water_sources_primary %in% c("water_kiosk", "vendors_shop", "piped", "prot_well_no_handpump", "prot_well_handpump")  ~ 1
+    water_sources_primary %in% c("water_kiosk", "vendors_shop", "piped", "prot_well_no_handpump", "prot_well_handpump", "water_tank_tap", "water_trucking_distrib", "borehole_pump")  ~ 1
     
   ),
   
@@ -200,12 +223,13 @@ data_indicators <- data %>% mutate (
   
   #### WASH Non critical index 6
   ###YS as above, could you share the listof improved/unimproved source of water.
+  ###ASSIL I shared the list on the excel file and updated the vectors here 
   wash_nc_index6 = case_when(
     sm_selected(water_access_distance_min,exactly = "more_60") |
-      water_sources_primary %in% c("unprot_well", "berkad", "water_tank_tap", "water_trucking_distrib", "borehole_pump")  ~ 1,
+      water_sources_primary %in% c("unprot_well", "berkad", "river")  ~ 1,
     
     sm_selected(water_access_distance_min,any = c("less_15","15_30","30_60") ) &
-      water_sources_primary %in% c("water_kiosk", "vendors_shop", "piped", "prot_well_no_handpump", "prot_well_handpump")  ~ 0,
+      water_sources_primary %in% c("water_kiosk", "vendors_shop", "piped", "prot_well_no_handpump", "prot_well_handpump", "water_tank_tap", "water_trucking_distrib", "borehole_pump")  ~ 0,
     
 
   ),
@@ -248,6 +272,7 @@ data_indicators <- data %>% mutate (
                      health_problems.injuries,
                      health_problems.measles)))==2 ~ 2,
     ###YS should be <= 1
+    ###YS You are right 
     rowSums(across(c(health_problems.malaria,
                      health_problems.fever,
                      health_problems.awd_cholera,
@@ -255,7 +280,7 @@ data_indicators <- data %>% mutate (
                      health_problems.malnutrition,
                      health_problems.gastrointernal,
                      health_problems.injuries,
-                     health_problems.measles)))==1 ~ 1,
+                     health_problems.measles)))<=1 ~ 1,
   ),
 
   #### HLT index 3
@@ -332,14 +357,15 @@ data_indicators <- data %>% mutate (
   
   ###YS this is a critical indicator
   #### HLT Non Critical index 9
-  health_nc_index9 = case_when(
-    health_women_unskilledhealthpersonnel %in% c("all","many","some") ~ 1,
-    health_women_unskilledhealthpersonnel %in% c("few","none") ~ 0,
-  ),
+  ### Deleted
+  # health_nc_index9 = case_when(
+  #   health_women_unskilledhealthpersonnel %in% c("all","many","some") ~ 1,
+  #   health_women_unskilledhealthpersonnel %in% c("few","none") ~ 0,
+  # ),
   
-  #### HLT Non Critical index 10
+  #### HLT Non Critical index 9
   ###YS ok
-  health_nc_index10 = case_when(
+  health_nc_index9 = case_when(
     sm_selected(health_barriers, any= c("cost", 
                                         "no_qualified", 
                                         "documents",
@@ -355,10 +381,13 @@ data_indicators <- data %>% mutate (
   
   ##### Nutrition - Critical ##### 
   #### Nutrition Critical index 1
-  ###YS it should be from 1 to 4
+  ### YS it should be from 1 to 4
+  ###ASSIl Corrected 
   nutrition_index1 = case_when(
-    nutrition_prop_malnurish %in% c("all","many","some") ~ 1,
-    nutrition_prop_malnurish %in% c("few","none") ~ 0,
+    nutrition_prop_malnurish %in% c("all") ~ 5,
+    nutrition_prop_malnurish %in% c("many") ~ 4,
+    nutrition_prop_malnurish %in% c("few") ~ 3,
+    nutrition_prop_malnurish %in% c("none") ~ 0,
   ),
   
   ##### Nutrition - Non Critical ##### 
@@ -387,6 +416,7 @@ data_indicators <- data %>% mutate (
   
   #### FSL Critical index 2
   ###YS: OK  but hunger_worst has a BIG trend
+  ###Assil I'll check that
   fs_index2 = case_when(
     hunger_level %in% c("hunger_worst") ~ 5,
     hunger_level %in% c("hunger_severe") ~ 4,
@@ -438,20 +468,22 @@ data_indicators <- data %>% mutate (
   ##### Education -  Critical ##### 
   #### Education Critical index 1
   ###YS education should not have a score of 4. 
+  ###ASSIL Corrected 
   education_index1 = case_when(
-    sm_selected(education_barriers_boys,any = c("child_recruited_ag", "displacement_conflict")) ~ 4,
-    sm_selected(education_barriers_boys,any = c("security_concerns", "child_lack_documentation", "no_gender_separ", "help_at_home", "costs", "child_pycho_distress", "work_outside_home", "flood", "marriage_pregnant")) ~ 3,
-    sm_selected(education_barriers_boys,any = c("far_away", "closed", "poor_infrastructure", "lack_quali_staff", "no_wash_at_school", "overcrowded") ) ~ 2,
-    sm_selected(education_barriers_boys,any = c("no_problem", "no_aware_education_opportunities", "parents_no_value_edu", "parents_no_approve_curric", "cultural_beliefs", "language")) ~ 1,
+    sm_selected(education_barriers_boys,any = c("child_recruited_ag", "displacement_conflict")) ~ 3,
+    sm_selected(education_barriers_boys,any = c("security_concerns", "child_lack_documentation", "no_gender_separ", "help_at_home", "costs", "child_pycho_distress", "work_outside_home", "flood", "marriage_pregnant")) ~ 2,
+    sm_selected(education_barriers_boys,any = c("no_problem", "no_aware_education_opportunities", "parents_no_value_edu", "parents_no_approve_curric", "cultural_beliefs", "language",
+                                                "far_away", "closed", "poor_infrastructure", "lack_quali_staff", "no_wash_at_school", "overcrowded")) ~ 1,
   ),
   
   #### Education Critical index 2
   ###YS education should not have a score of 4. 
+  ###ASSIL Corrected 
   education_index2 = case_when(
-    sm_selected(education_barriers_girls,any = c("child_recruited_ag", "displacement_conflict")) ~ 4,
-    sm_selected(education_barriers_girls,any = c("security_concerns", "child_lack_documentation", "no_gender_separ", "help_at_home", "costs", "child_pycho_distress", "work_outside_home", "flood", "marriage_pregnant")) ~ 3,
-    sm_selected(education_barriers_girls,any = c("far_away", "closed", "poor_infrastructure", "lack_quali_staff", "no_wash_at_school", "overcrowded") ) ~ 2,
-    sm_selected(education_barriers_girls,any = c("no_problem", "no_aware_education_opportunities", "parents_no_value_edu", "parents_no_approve_curric", "cultural_beliefs", "language")) ~ 1,
+    sm_selected(education_barriers_girls,any = c("child_recruited_ag", "displacement_conflict")) ~ 3,
+    sm_selected(education_barriers_girls,any = c("security_concerns", "child_lack_documentation", "no_gender_separ", "help_at_home", "costs", "child_pycho_distress", "work_outside_home", "flood", "marriage_pregnant")) ~ 2,
+    sm_selected(education_barriers_girls,any = c("no_problem", "no_aware_education_opportunities", "parents_no_value_edu", "parents_no_approve_curric", "cultural_beliefs", "language",
+                                                 "far_away", "closed", "poor_infrastructure", "lack_quali_staff", "no_wash_at_school", "overcrowded")) ~ 1,
   ),
   
   
@@ -592,8 +624,7 @@ wash_non_critical_indicators <- c("wash_nc_index1", "wash_nc_index2", "wash_nc_i
 health_critical_indicators <- c("health_index1","health_index2","health_index3")
 health_non_critical_indicators <- c("health_nc_index1",
                                     "health_nc_index2", "health_nc_index3", "health_nc_index4", "health_nc_index5",
-                                    "health_nc_index6", "health_nc_index7", "health_nc_index8", "health_nc_index9",
-                                    "health_nc_index10")
+                                    "health_nc_index6", "health_nc_index7", "health_nc_index8", "health_nc_index9")
 
 nutrition_critical_indicators <- c("nutrition_index1")
 nutrition_non_critical_indicators <- c("nutrition_nc_index1", "nutrition_nc_index2")
@@ -611,63 +642,44 @@ education_non_critical_indicators <- c("education_nc_index1","education_nc_index
 protection_critical_indicators <- c("protection_index1","protection_index2")
 protection_non_critical_indicators <- c("protection_nc_index1", "protection_nc_index2", "protection_nc_index3", "protection_nc_index4")
 
+score_mean <- function(x) {
+  case_when(x >2/3 ~ 3,
+            x > 1/3 ~ 2,
+            x <= 1/3 ~ 1)
+}
 
-data_indicators <- data_indicators %>% 
+data_indicators <-  data_indicators %>% 
+  mutate(
+    mean_nc_snfi = score_mean(rowMeans(select(.,!!!syms(snfi_non_critical_indicators)), na.rm = T)),
+    mean_nc_wash = score_mean(rowMeans(select(.,!!!syms(wash_non_critical_indicators)), na.rm = T)),
+    mean_nc_hlt = score_mean(rowMeans(select(.,!!!syms(health_non_critical_indicators)), na.rm = T)),
+    mean_nc_nutrition = score_mean(rowMeans(select(.,!!!syms(nutrition_non_critical_indicators)), na.rm = T)),
+    mean_nc_fs = score_mean(rowMeans(select(.,!!!syms(fs_non_critical_indicators)), na.rm = T)),
+    mean_nc_edu = score_mean(rowMeans(select(.,!!!syms(education_non_critical_indicators)), na.rm = T)),
+    mean_nc_protection = score_mean(rowMeans(select(.,!!!syms(protection_non_critical_indicators)), na.rm = T))
+    
+  ) 
+
+
+data_indicators <-  data_indicators %>% 
   
   mutate(
     
-    snfi_score = pmax(ifelse(rowMeans(select(.,!!!syms(snfi_non_critical_indicators)), na.rm = T)<0.99,
-                                  ifelse(floor(((round(rowMeans(select(.,!!!syms(snfi_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1)>0,
-                                         floor(((round(rowMeans(select(.,!!!syms(snfi_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1),1)
-                                  ,
-                                  3),na.rm = T),
+    snfi_score = mean_nc_snfi,
     
-    
-    wash_score = pmax(!!!syms(wash_critical_indicators),
-                      ifelse(rowMeans(select(.,!!!syms(wash_non_critical_indicators)), na.rm = T)<0.99,
-                             ifelse(floor(((round(rowMeans(select(.,!!!syms(wash_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1)>0,
-                                    floor(((round(rowMeans(select(.,!!!syms(wash_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1),1)
-                             ,
-                             3),na.rm = T),
-    
-    
-    protection_score = pmax(!!!syms(protection_critical_indicators),
-                            ifelse(rowMeans(select(.,!!!syms(protection_non_critical_indicators)), na.rm = T)<0.99,
-                                   ifelse(floor(((round(rowMeans(select(.,!!!syms(protection_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1)>0,
-                                          floor(((round(rowMeans(select(.,!!!syms(protection_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1),1)
-                                   ,
-                                   3),na.rm = T),
-    
-    fs_score = pmax(!!!syms(fs_critical_indicators),
-                    ifelse(rowMeans(select(.,!!!syms(fs_non_critical_indicators)), na.rm = T)<0.99,
-                           ifelse(floor(((round(rowMeans(select(.,!!!syms(fs_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1)>0,
-                                  floor(((round(rowMeans(select(.,!!!syms(fs_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1),1)
-                           ,
-                           3),na.rm = T),
-    
-    
-    health_score = pmax(!!!syms(health_critical_indicators),
-                        ifelse(rowMeans(select(.,!!!syms(health_non_critical_indicators)), na.rm = T)<0.99,
-                               ifelse(floor(((round(rowMeans(select(.,!!!syms(health_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1)>0,
-                                      floor(((round(rowMeans(select(.,!!!syms(health_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1),1)
-                               ,
-                               3),na.rm = T),
-    
-    nutrition_score = pmax(!!!syms(nutrition_critical_indicators),
-                    ifelse(rowMeans(select(.,!!!syms(nutrition_non_critical_indicators)), na.rm = T)<0.99,
-                           ifelse(floor(((round(rowMeans(select(.,!!!syms(nutrition_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1)>0,
-                                  floor(((round(rowMeans(select(.,!!!syms(nutrition_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1),1)
-                           ,
-                           3),na.rm = T),
-    
-    
-    education_score = pmax(!!!syms(education_critical_indicators),
-                           ifelse(rowMeans(select(.,!!!syms(education_non_critical_indicators)), na.rm = T)<0.99,
-                                  ifelse(floor(((round(rowMeans(select(.,!!!syms(education_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1)>0,
-                                         floor(((round(rowMeans(select(.,!!!syms(education_non_critical_indicators)), na.rm = T),digits = 2)-0.001)/0.3333)+1),1)
-                                  ,
-                                  3),na.rm = T),
-  )
+    wash_score = pmax(!!!syms(wash_critical_indicators),mean_nc_wash,na.rm = T),
+  
+    protection_score = pmax(!!!syms(protection_critical_indicators),mean_nc_protection,na.rm = T),
+                          
+    fs_score = pmax(!!!syms(fs_critical_indicators),mean_nc_fs,na.rm = T),
+                   
+    health_score = pmax(!!!syms(health_critical_indicators),mean_nc_hlt,na.rm = T),
+                     
+    nutrition_score = pmax(!!!syms(nutrition_critical_indicators),mean_nc_nutrition,na.rm = T),
+
+    education_score = pmax(!!!syms(education_critical_indicators),mean_nc_edu,na.rm = T)
+                       
+  ) 
 
 data_indicators <- data_indicators %>% 
   
@@ -700,8 +712,6 @@ data_indicators <- data_indicators %>%
     health_need = case_when(
       health_score <= 2 ~ 0,
       health_score >= 3 ~ 1),
-    
-  
   
   )
 
@@ -713,9 +723,14 @@ data_indicators <- data_indicators %>%
   )
 
 
+output <- data_indicators %>% select(
+                          matches("index[[:digit:]]"),
+                          ends_with("_score"),
+                          ends_with("_need"),
+                          number_of_needs) 
 
 
-write.csv(data_indicators,'LSG_output_12_04.csv',row.names = F)
+write.csv(output,'LSG_output_15_04.csv',row.names = F)
 
 
 
